ada7cbb8346dd341eab2a04c98464037b5d0e8a3
EC2 Default User <ec2-user@ip-172-31-21-121.us-east-2.compute.internal>
2022-02-17 23:06:14 +0000

    first testing after jenkins integration

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Echo "Sample shell script"
+Echo "ignore below"
+//This command kills all previous vino instances
+//sudo killall vino-server
+//This command forces the proper release of the display
+//export DISPLAY=: 0.0
+//This command starts vino again.
+/usr/lib/vino/
