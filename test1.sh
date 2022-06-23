fa86d627742741d6b06d318e085fdac7f4f3bf36
ramyaparepalli <ramya.parepalli@gmail.com>
2022-06-23 05:02:08 +0000

    added changes

--- test1.sh
+++ test1.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 echo "testing github- jenkins integration"
-
+echo " edited this file with userone and connected to the git from ec2 instance and committed the changes to git repository"
